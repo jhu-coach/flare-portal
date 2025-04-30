5a041e1f3ef0136d38a5e7ab41b74024d7383f35
jhu-coach <132102165+jhu-coach@users.noreply.github.com>
2025-04-30 09:36:06 -0400

    Update production.py for CORS and CSRF

--- flare_portal/settings/production.py
+++ flare_portal/settings/production.py
@@ -14,3 +14,23 @@ SESSION_COOKIE_SECURE = True
 # Ensure that the CSRF cookie is only sent by browsers under an HTTPS connection.
 # https://docs.djangoproject.com/en/stable/ref/settings/#csrf-cookie-secure
 CSRF_COOKIE_SECURE = True
+
+
+# CORS and CSRF settings
+INSTALLED_APPS += ["corsheaders"]
+MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
+
+CORS_ALLOWED_ORIGINS = [
+    "https://flare-portal-4cd977e0207e.herokuapp.com",  # Replace with your real URL
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://flare-portal-4cd977e0207e.herokuapp.com",
+]
+
+# Optional: Recommended HTTPS security settings
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
